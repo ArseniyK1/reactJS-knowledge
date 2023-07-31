17c283d2856cf9f9ab3593a549fdf2dba04087ab
ArseniyK1 <qwertototo2332@gmail.com>
2023-07-31 15:22:41 +0300

    added close modal event key escape

--- src/UI/MyModal/MyModal.jsx
+++ src/UI/MyModal/MyModal.jsx
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
 import styles from "./MyModal.module.css";
 
 const MyModal = ({ children, visible, setVisible }) => {
   const rootClasses = [styles.myModal];
+
+  // Обработчик события для нажатия клавиши "Escape"
+  const handleEscapeKeyPress = (event) => {
+    if (event.key === "Escape") {
+      setVisible(false);
+    }
+  };
+
+  // Эффект для добавления обработчика события при появлении модального окна
+  useEffect(() => {
+    if (visible) {
+      window.addEventListener("keydown", handleEscapeKeyPress);
+    }
+
+    // Очистка обработчика события при закрытии модального окна или при размонтировании компонента
+    return () => {
+      window.removeEventListener("keydown", handleEscapeKeyPress);
+    };
+  }, [visible]);
 
   if (visible) {
     rootClasses.push(styles.active);
